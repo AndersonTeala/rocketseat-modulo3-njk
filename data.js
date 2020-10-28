0a6f4a583f50c101d85aded008e42b231c8c8880
Anderson Teala <andersonteala@gmail.com>
2020-10-28 16:58:32 -0300

    Create new files and update

---
+++ data.js
@@ -0,0 +1,39 @@
+module.exports = [
+    {
+        id: "XcU9GEUZTQA",
+        title: "Aprenda Git e Github na prática | Anderson Teala",
+        duration: "57 min",
+        price: "Free",
+        image: "https://blogatnteala.sentidoconsultoria.com.br/wp-content/uploads/2020/10/1.png",
+    },
+    {
+        id: "XcU9GEUZTQA",
+        title: "Aprenda CSS na prática | Anderson Teala",
+        duration: "37 min",
+        price: "Free",
+    },
+    {
+        id: "XcU9GEUZTQA",
+        title: "Aprenda HTML 5 na prática | Anderson Teala",
+        duration: "42 min",
+        price: "Free",
+    },
+    {
+        id: "XcU9GEUZTQA",
+        title: "Aprenda Node.JS na prática | Anderson Teala",
+        duration: "37 min",
+        price: "Free",
+    },
+    {
+        id: "XcU9GEUZTQA",
+        title: "Aprenda Python na prática | Anderson Teala",
+        duration: "77 min",
+        price: "Free",
+    },
+    {
+        id: "XcU9GEUZTQA",
+        title: "Aprenda React.JS na prática | Anderson Teala",
+        duration: "57 min",
+        price: "Free",
+    },
+]
--- server.js
+++ server.js
@@ -2,21 +2,36 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const server = express()
+const videos = require("./data");
 
 server.use(express.static('public'))
 
 server.set("view engine", "njk")
 
 nunjucks.configure("views", {
-    express: server
+    express: server,
+    autoescape: false
 })
 
 server.get("/", function (req, res) {
-    return res.render("about")
+    const about = {
+        avatar_url: "https://atnteala.sentidoconsultoria.com.br/wp-content/uploads/2020/05/69f162f4-6126-4c87-94dd-091a83a23f44-300x300-1.jpeg",
+        name: "Anderson Teala",
+        role: "Instrutor - Tecsec",
+        description: 'Programador full-stack, focado em trazer o melhor ensino para iniciantes em programação. Colaborador da <a href="https://atnteala.sentidoconsultoria.com.br/" target="_blank">Tecsec</a>',
+        links: [
+            { name: "Github", url: "https://github.com/AndersonTeala" },
+            { name: "Instagram", url: "https://github.com/AndersonTeala" },
+            { name: "Linkedin", url: "https://www.linkedin.com/in/anderson-teala-50a1a5167/" },
+        ]
+    }
+
+    return res.render("about", { about })
 })
 
 server.get("/portfolio", function (req, res) {
-    return res.render("portfolio")
+
+    return res.render("portfolio", { items: videos })
 })
 
 server.listen(5000, function () {
